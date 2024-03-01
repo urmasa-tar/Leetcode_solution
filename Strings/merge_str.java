b672905bea1c6bb24ceab00929fb9f97ff1b4001
urmasa-tar <nik.goltsev.97@bk.ru>
2024-03-01 13:47:59 +0700

    Auto_save

--- Strings/merge_str.java
+++ Strings/merge_str.java
@@ -1,27 +1,50 @@
 import java.util.Scanner;
-import java.util.Strings;
 
 
 public class merge_str {
     
     public static void main(String[] args){
-        // ---> meow(14);
 
-        // Scanner example
-        Scanner in = new Scanner(System.in);
-
-        int mw_num = in.nextInt();
-
-        meow(mw_num);
-
-        in.close();
-        // <---
+        mergeAlternately("abc", "def");
         
     }
     
     public static void meow(int n){
         for(int i = 0; i < n; i++){
-            System.out.println("meow\n");
+            System.out.print("meow\n");
         }
+    }
+
+    public static String mergeAlternately(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int i = 0;
+        int j = 0;
+        String res = "";
+
+        // in cycle add chares by i and j
+        while (true) { // How to decompose that function?
+            if(i < n && j < m){
+                if(i <= j){
+                    res = res.concat(Character.toString(word1.charAt(i)));
+                    i += 1;
+                }else{
+                    res = res.concat(Character.toString(word2.charAt(j)));
+                    j += 1;
+                }
+            }else if(i < n){
+                res = res.concat(Character.toString(word1.charAt(i)));
+                i += 1;
+            }else if(j < m){
+                res = res.concat(Character.toString(word2.charAt(j)));
+                j += 1;
+            }else{
+                break;
+            }
+        }
+
+
+        System.out.print(res);
+        return res;
     }
 }
