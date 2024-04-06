9b6f95e4ffef722fc62fd482c2cd224e38b7591b
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-06 15:08:26 +0300

    Spiral_matrix_file

---
+++ Process_simulation/spirial_matrix.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class spirial_matrix{
+
+    public static void main(String[] args){
+
+    }
+
+    public List<Integer> SpiralOrder(int[][] matrix) {
+
+        List<Integer> list_of_spiral = new ArrayList<>();
+
+        return list_of_spiral;
+    }
+
+}
