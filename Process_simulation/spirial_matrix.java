d2b25576778c8a0ed3cc2eca012afabe354ffb3a
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-06 15:33:04 +0300

    maket

--- Process_simulation/spirial_matrix.java
+++ Process_simulation/spirial_matrix.java
@@ -10,6 +10,46 @@ class spirial_matrix{
     public List<Integer> SpiralOrder(int[][] matrix) {
 
         List<Integer> list_of_spiral = new ArrayList<>();
+        int m = matrix[0].length; // - in row
+        int n = matrix.length; // - number of rows
+        int i, j = 0;
+        char struct = 'r';// r -> d -> l -> u
+        byte in_row = 0; // break status
+
+        list_of_spiral.add(matrix[0][0]);
+
+        while (true){
+
+            switch (struct){
+                    case ('r'):
+
+
+
+                        break;
+
+                    case ('d'):
+
+
+
+                        break;
+
+                    case ('l'):
+
+                        break;
+
+                    case ('u'):
+
+                        break;
+
+                default:
+
+                    //error moument
+                    break;
+            }
+
+
+            if(in_row == 4)break;
+        }
 
         return list_of_spiral;
     }
