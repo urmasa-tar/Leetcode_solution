7755ee726b4f3e17f0b777aa3384a4d6e6678eaa
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-10 16:08:47 +0300

    With_cycle_for_median

--- Process_simulation/two_arrays_median.java
+++ Process_simulation/two_arrays_median.java
@@ -14,8 +14,25 @@ public class two_arrays_median {
 
         int i = 0;
         int j = 0;
-
         int[] arr = new int[n + m];
+
+        while (true){
+            if((i >= n) && ( j >= m)){
+                break;
+            }else if(i >= n){
+
+            }else if(j >= m){
+
+            }else{
+                if(nums1[i] > nums2[j]){
+                    arr[i + j] = nums2[j];
+                    j += 1;
+                }else{
+                    arr[i + j] = nums1[i];
+                    i += 1;
+                }
+            }
+        }
 
         return res;
 
