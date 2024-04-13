8681af4ff65b1b21ab712e6153ce84169c728908
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-04-13 13:22:35 +0300

    My_rectangle_start

--- Process_simulation/My_Rectanle.java
+++ Process_simulation/My_Rectanle.java
@@ -1,7 +1,37 @@
+import java.util.Arrays;
+
 public class My_Rectanle {
 
     public int maximalRectangle(char[][] matrix) {
+        int res;
 
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            res = 0;
+        }else{
+            res = 0;
+
+            int n = matrix.length;
+            int m = matrix[0].length;
+
+            byte[][] stat_arr = new byte[n][m];
+
+            for(int i = 0; i < n; i++){
+                for(int j = 0; j < m; j++){
+                    
+                }
+            }
+
+        }
+
+
+
+        return res;
+    }
+
+    public byte[][] status_in_row(char[][] input_mat, byte[][] stat){
+        byte[][] stat_cop = Arrays.copyOf(stat, stat.length);
+
+        return stat_cop;
     }
 
 }
