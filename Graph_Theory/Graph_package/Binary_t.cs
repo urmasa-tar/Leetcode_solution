d242902193f380dead7e416de7cffcd83cc4beff
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-06-22 15:54:45 +0300

    files_with_code

---
+++ Graph_Theory/Graph_package/Binary_t.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_package
+{
+    internal class Binary_t
+    {
+    }
+}
