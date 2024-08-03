c46db08a885c0c684ba616fbf6b6ce260829f9fb
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-08-03 15:12:44 +0300

    MeethodForDifferenceSearch

--- Graph_Theory/Graph_package/GeneticMutation.cs
+++ Graph_Theory/Graph_package/GeneticMutation.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Graph_package
 {
-    internal class GeneticMutation
+    public class GeneticMutation
     {
+        public GeneticMutation() { }
+
+        public int MinMutation(string startGene, string endGene, string[] bank)
+        {
+            int res = 0;
+            int[][] matrixOfLength = new int[bank.Length + 2][];
+            for(int i = 0; i < bank.Length + 2; i++)
+            {
+                if(i == 0)
+                {
+                    matrixOfLength[i] = new int[bank.Length + 2];
+                    for(int anotherStr = 0; anotherStr < bank.Length + 2; anotherStr++)
+                    {
+                        if((anotherStr != i) &(anotherStr != bank.Length + 1))
+                        {
+                            matrixOfLength[i][anotherStr] = DifBetwenStr(startGene, bank[anotherStr - 1]);
+                        }
+                        else
+                        {
+                            matrixOfLength[i][anotherStr] = 0;
+                        }
+                    }
+                }else if(i == bank.Length - 1)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            return res;
+        }
+
+        public int DifBetwenStr(string str1, string str2)
+        {
+            int res = -1;
+
+            return res;
+        }
     }
+
 }
