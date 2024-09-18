3f231120e6d3cecdadab87dedc7b18b79448744d
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-09-18 11:54:01 +0300

    robberTask

---
+++ Dynamical_prog/rober.cs
@@ -0,0 +1,21 @@
+
+
+public class rober{
+
+    public int Rob(int[] nums) {
+        // need to create arr for mx sum
+        int[] arrS = new int[nums.length];
+        long houseS = 0;
+        for(int house = 0; i < nums.length; i++){
+
+        }
+    }
+
+    protected int maxInMoument(int house, int[] nums){
+        int res = 0;
+        for(int i = 0; i < house - 1; i++){
+            
+        }
+    }
+
+}
