83c2f6e4556bcf94eea3ead82e2ac3d408add87a
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-01-01 02:45:03 +0300

    AddTwoNums

---
+++ Math/ReverseInt/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int num;
+        string input = Console.ReadLine();
+
+        int.TryParse(input, out num);
+        Console.WriteLine(calcRev(num));
+    }
+
+    public static int calcRev(int num)
+    {
+        int res = 0;
+        int cpNum = num;
+        while (true)
+        {
+            if((cpNum > 0) && (cpNum % 10 > 0))
+            {
+                res = res * 10 + cpNum % 10;
+                cpNum = cpNum / 10; 
+            }else if(cpNum <= 0){ break; }
+        }
+
+        return res;
+    }
+}
