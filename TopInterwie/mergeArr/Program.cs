496f5bebf69a077a7d1f59d686fe364722175b71
urmasa <nik.goltsev.97@bk.ru>
2025-02-02 19:17:35 +0300

    First solution

--- TopInterwie/mergeArr/Program.cs
+++ TopInterwie/mergeArr/Program.cs
@@ -1,16 +1,21 @@
-﻿namespace mergeArr{
-    class Program{
-        public static void Main(string[] args){
+﻿using System;
 
-            int[] arr1 = new int[4]{1, 2, 3, 4};
-            int[] arr2 = new int[]{1};
+namespace mergeArr
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = new int[4] { 1, 2, 3, 4 };
+            int[] arr2 = new int[] { 1 };
 
             Solution solution = new Solution();
 
             solution.Merge(arr1, 3, arr2, 1);
 
-            for(int i = 0; i < arr1.Length; i++){
-                Console.Write(arr1[i] + ' ');
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.Write(arr1[i] + " ");
             }
         }
     }
--- TopInterwie/mergeArr/solution.cs
+++ TopInterwie/mergeArr/solution.cs
@@ -1,54 +1,46 @@
-namespace mergeArr{
-
-    public class Solution {
-
-        public void Merge(int[] nums1, int m, int[] nums2, int n) {
-            int i = 0;
-            int j = 0;
-            while(true){
-                if((i >= m) && (j >= n)){ break; }
+namespace mergeArr
+{
+    public class Solution
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] newArr = new int[n + m];
+            int i = 0, j = 0;
+            while (true)
+            {
+                if ((i >= m) && (j >= n))
+                {
+                    break;
+                }
+                if (i >= m)
+                {
+                    newArr[i + j] = nums2[j];
+                    j += 1;
+                }
+                else if (j >= n)
+                {
+                    newArr[i + j] = nums1[i];
+                    i += 1;
+                }
                 else
                 {
-                    if( i >= m){
-                        nums1 = moveToRight(nums1, i + j, nums2[j], i + j + 1);
-                        j += 1;
+                    if (nums1[i] <= nums2[j])
+                    {
+                        newArr[i + j] = nums1[i];
                         i += 1;
-                    }else if(j >= n){
-                        break;
-                    }else{
-                        if(nums1[i] < nums2[j]){
-                            i += 1;
-                        }else{
-                            nums1 = moveToRight(nums1, i + j, nums2[j], i + j + 1);
-                            j += 1;
-                            i += 1;
-                        }
                     }
-                }
-            }   
-        }
-
-        public int[] moveToRight(int[] nums1, int from, int newN, int newLength){
-
-            int skipedNum = -1;
-            for(int i = 0; i < newLength; i++){
-                if(i < from){
-                    continue;
-                }else{
-                    if(skipedNum == -1){
-                        skipedNum = nums1[i];
-                        nums1[i] = newN;
-                    }else{
-                        int p = nums1[i];
-                        nums1[i] = skipedNum;
-                        skipedNum = p;
+                    else
+                    {
+                        newArr[i + j] = nums2[j];
+                        j += 1;
                     }
                 }
             }
 
-            return nums1;
+            for (int k = 0; k < newArr.Length; k++)
+            {
+                nums1[k] = newArr[k];
+            }
         }
-
-
     }
 }
