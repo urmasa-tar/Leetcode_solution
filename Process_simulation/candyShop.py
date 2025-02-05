b322a7e9cb4990698d9d572970db0055aea412fb
urmasa <nik.goltsev.97@bk.ru>
2025-02-06 01:26:26 +0300

    candyShop

--- Process_simulation/candyShop.py
+++ Process_simulation/candyShop.py
@@ -5,31 +5,32 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         
         res = 0
-        index_list = self.ind_of_sorted_arr()
+        index_list = self.ind_of_sorted_arr(ratings)
         candy_count = [0 for i in range(len(ratings))]
         for index in index_list:
             
-            need_c = self.min_by_neibor()
+            need_c = self.min_by_neibor(index, ratings, candy_count)
+            candy_count[index] = need_c
+            res += need_c
 
         return res
 
-    def min_by_neibor(self, index, arr, arr_count) -> int:
-
-        if(index == 0):
+    def min_by_neibor(self, index, arr_rat, arr_count) -> int:
+        res = 1
+        if(index != 0):
             #
-        elif(index == len(arr)):
+            if(arr_rat[index - 1] < arr_rat[index] ):
+                res = arr_count[index - 1] + 1
+        if(index != len(arr_rat) - 1):
             #
-        else:
-            #
-
-        return None
+            if(arr_rat[index] > arr_rat[index + 1]):
+                res = max(res, arr_count[index + 1] + 1)
+        return res
 
     def ind_of_sorted_arr(self, arr: List[int]) -> List[int]:
-
         indexed_list = [(value, index) for index, value in enumerate(arr)]
         sorted_indexed_list = sorted(indexed_list, key=lambda x: x[0])
         index_res = [index for value, index in sorted_indexed_list]
-
         return index_res
 
 def main():
