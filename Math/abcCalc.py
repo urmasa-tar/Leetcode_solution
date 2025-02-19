331e42af1244c9990a8a2eeaf7ec3c2f0c57d349
urmasa <nik.goltsev.97@bk.ru>
2025-02-19 16:12:39 +0300

    Trial_Of_ABCSwap

---
+++ Math/abcCalc.py
@@ -0,0 +1,11 @@
+class Solution:
+
+    arr_abc = ['a', 'b', 'c']
+
+    def recurebtList(len: int, ) -> str:
+
+
+
+    def getHappyString(self, n: int, k: int) -> str:
+        
+        return None
