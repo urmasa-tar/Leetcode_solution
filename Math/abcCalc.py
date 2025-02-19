f2fa25fb529a342a7969dc69d660c67e1c80657f
urmasa <nik.goltsev.97@bk.ru>
2025-02-19 16:13:12 +0300

    Trial_Of_ABCSwap

--- Math/abcCalc.py
+++ Math/abcCalc.py
@@ -1,11 +1,17 @@
 class Solution:
-
-    arr_abc = ['a', 'b', 'c']
-
-    def recurebtList(len: int, ) -> str:
-
-
-
     def getHappyString(self, n: int, k: int) -> str:
+        def dfs(prefix, n, k):
+            if n == 0:
+                return prefix
+            for c in 'abc':
+                if prefix and c == prefix[-1]:
+                    continue
+                cnt = 2 ** (n2 - len(prefix) - 1)
+                if cnt >= k:
+                    return dfs(prefix + c, n - 1, k)
+                else:
+                    k -= cnt
+            return ""
         
-        return None
+        n2 = n
+        return dfs("", n, k)
