0df607aeba19fdc8a932ec2d5c78dad6f99b7cd9
urmasa <nik.goltsev.97@bk.ru>
2025-02-22 07:56:13 +0300

    Try of jump task

---
+++ Dynamical_prog/jumpCan.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+
+    def canJump(self, nums: List[int]) -> bool:
+        res = False
+
+        index = 0
+        while(True):
+            if(index == -1):
+                break
+            elif(index == (len(nums) - 1)):
+                res = True
+                break
+            index = self.max_Jump__in_subarr(index, nums[index], nums)
+            
+        return res
+    
+    def max_Jump__in_subarr(self, i: int, step: int, nums: List[int]) -> int:
+        res = -1
+        for ind in range(i + 1, i + step + 1):
+            if(ind == len(nums) - 1):
+                res = ind
+                break
+            if((nums[ind] > 0) and (nums[ind] + ind > res)):
+                res = ind
+
+        return res
+
+if __name__ == "__main__":
+    calc = Solution()
+    nums = [int(i) for i in input().split()]
+    print(calc.canJump(nums))
