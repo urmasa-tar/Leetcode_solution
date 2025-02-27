009786b866999a83e77a684f7d347b1813f046a8
urmasa <nik.goltsev.97@bk.ru>
2025-02-27 18:05:16 +0300

    First try for substr

--- Process_simulation/bigestSubString.py
+++ Process_simulation/bigestSubString.py
@@ -1,3 +1,5 @@
+
+# Bad calc time
 class Solution:
 
     def longestPalindrome(self, s: str) -> str:
@@ -9,24 +11,34 @@ class Solution:
             for j in range(len(s) - 1, ind, -1):
                 if(s[ind] == s[j]):     
                     if(self.check_sub_str(s, ind, j)):
-                        res = self.bigger_str(res, s[ind:j]) # How to get sub str !
+                        res = self.bigger_str(res, s[ind : j + 1]) # How to get sub str !
                 else:
                     continue
         return res
     
     def check_sub_str(self, s: str, start: int, end: int) -> bool:
         res = True
-        
+        j = 0
+        for i in range(start, end):
+            if(i == end - j):
+                break
+            elif(s[i] == s[end - j]):
+                j += 1
+            else:
+                res = False
+
         return res
 
     def bigger_str(self, s1: str, s2: str):
         res = s1
-
-        return s1
+        if(len(s2) > len(s1)):
+            res = s2
+        return res
 
 if __name__ == '__main__':
 
     calc = Solution()
 
-    st_test = input()
+    # st_test = input()
+    st_test = 'erertrere'
     print(calc.longestPalindrome(st_test))
