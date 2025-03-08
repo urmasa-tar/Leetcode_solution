b820ee93486c4b6969528900e08e8ec00f3f4803
urmasa <nik.goltsev.97@bk.ru>
2025-03-08 16:42:31 +0300

    TwoIndex

---
+++ Process_simulation/TwoSum2.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        res = []
+        i = 0
+        j = len(numbers) - 1
+        while(True):
+            if(numbers[i] + numbers[j] == target):
+                res.append(i + 1)
+                res.append(j + 1)
+                break
+            elif(numbers[i] + numbers[j] > target):
+                i += 1
+                if(i >= j):
+                    break
+            else:
+                j -= 1
+                if(i >= j):
+                    break
+        return res
+    
+if __name__ == "__main__":
+    calc = Solution()
