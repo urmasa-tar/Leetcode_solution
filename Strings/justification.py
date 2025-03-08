fb8f0abc1b732b100605c6fda1d931749b452088
urmasa <nik.goltsev.97@bk.ru>
2025-03-08 15:58:45 +0300

    strings

---
+++ Strings/justification.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def fullJustify(self, words, maxWidth):
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(w) + len(cur) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i%(len(cur)-1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
+        
+
+if __name__ == "__main__":
+
+    calc = Solution()
+    calc.fullJustify()
+
