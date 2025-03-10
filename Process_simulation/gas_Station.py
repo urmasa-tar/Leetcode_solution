950671356caf4b5e88518d82fadef0bfbdb64f56
urmasa <nik.goltsev.97@bk.ru>
2025-03-10 12:04:24 +0300

    Gas station res

--- Process_simulation/gas_Station.py
+++ Process_simulation/gas_Station.py
@@ -5,7 +5,26 @@ class Solution:
     def __init__(self):
         pass
 
-    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        res = 0
+    def rec_From_p(self, point: int, curr_gas: int, gas: List[int], cost: List[int]) -> int:
+        local_res += cost[point]
+        gas_now = curr_gas + gas[point]
+        if(point + curr_gas >= len(gas)):
+            local_res = 0
+        else:
+            local_res += cost[point]
 
-        return res
+        return local_res
+    
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+                
+        curernt_gas = 0
+        start = 0
+        for i in range(len(gas)):
+            curernt_gas += gas[i] - cost[i]
+            if curernt_gas < 0:
+                curernt_gas = 0
+                start = i + 1
+
+        return start
