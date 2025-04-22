779d3b86fc434e822988e6092eb1245ee030690b
urmasa <nik.goltsev.97@bk.ru>
2025-04-23 02:10:45 +0300

    is palindrom

---
+++ Process_simulation/validPalindrom/validPalindrom/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+ 
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(IsPalindrome("abccba"));
+        Console.WriteLine(IsPalindrome("abcdefg"));
+    }
+
+    public static bool IsPalindrome(string s)
+    {
+        String to_Check = update_Str(s);
+        bool res = false;
+        int start = 0;
+        // Len of string
+        int end = to_Check.Length - 1;
+        while (true)
+        {
+            if(end <= start)
+            {
+                res = true;
+                break;
+            }
+            else
+            {
+                if (to_Check[start] != to_Check[end])
+                {
+                    break;
+                }
+                else
+                {
+                    start++;
+                    end--;
+                }
+            }
+        }
+        return res;
+    }
+    
+    public static String update_Str(String s)
+    {
+        char[] charactersList = { ' ', '"', ',', '{', '}' };
+        String res = "";
+        foreach (char c in s)
+        {
+            if (!charactersList.Contains(c))
+            {
+                res += c;
+            }
+        }
+
+        return res;
+    }
+}
