66155dcab5160723d23d6948d13c6c6d99184b58
urmasa <nik.goltsev.97@bk.ru>
2025-04-26 12:49:27 +0300

    Sudoku program

--- Dynamical_prog/medianOf_TwoArrs.py
+++ Dynamical_prog/medianOf_TwoArrs.py
@@ -7,6 +7,7 @@ class Solution:
 
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
 
+        res = 0.1
         counted_num = 0
         if len(nums1) + len(nums2) % 2 != 0:
             counted_num = (len(nums1) + len(nums2)) // 2 + 1
@@ -20,8 +21,25 @@ class Solution:
 
             if (i_nums1 < len(nums1)) and (j_nums2 < len(nums2)):
 
-                if nums1[i_nums1] < nums2[j_nums2]:
-
+                if nums1[i_nums1] <= nums2[j_nums2]:
                     i_nums1 += 1
+                    if((i_nums1 + j_nums2) == counted_num):
+                        if(counted_num % 2 == 1):
+                            res = min(nums1[i_nums1], nums2[j_nums2]) # !! Danger moment
+                        else:
+                            res = float(nums1[i_nums1 - 1]) + 0.5
+                        break # Out from cicle
+                else:
+                    j_nums2 += 1
+                    if((i_nums1 + j_nums2) == counted_num):
+                        if(counted_num % 2 == 1):
+                            res = min(nums1[i_nums1], nums2[j_nums2]) # !!! May be out of range
+                        else:
+                            res = float(nums1[i_nums1 - 1]) + 0.5
+                        break # Out from cycle
+            elif(i_nums1 >= len(nums1)):
+                pass
+            else:
+                pass
 
-        return None
+        return res
---
+++ Dynamical_prog/validSudoku.py
@@ -0,0 +1,85 @@
+from typing import List
+
+
+class ValidTester:
+    
+    def __init__(self):
+        
+        self.dict_to_Str = {
+            1:"1",
+            2:"2",
+            3:"3",
+            4:"4",
+            5:"5",
+            6:"6",
+            7:"7",
+            8:"8",
+            9:"9"
+        }
+        
+        self.dict_to_Num = self.reverse_dict(self.dict_to_Str)
+        self.dict_to_Num["."] = 0
+
+    def reverse_dict(self, base_dc: dict) -> dict:
+        res = {}
+        for key, val in base_dc.items():
+            res[val] = key
+        return res    
+    
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res = False
+        
+        return res
+    
+    def rec_Cycle(self, st: int, col: int, board: List[List[str]]) -> bool:
+        res = False
+        
+        if(self.dict_to_Num[board[st][col]] == 0):
+            for i in range(1, 10):
+                board[st][col] = self.dict_to_Str[i]
+            if(col == 8) and (st == 8):
+                res = True
+            elif(col == 8):
+                res = self.rec_Cycle(st + 1, 0, board)
+            else:
+                res = self.rec_Cycle(st, col + 1, board)
+        else:
+            if(col == 8) and (st == 8):
+                res = True
+            elif(col == 8):
+                res = self.rec_Cycle(st + 1, 0, board)
+            else:
+                res = self.rec_Cycle(st, col + 1, board)
+        
+        
+        return res
+    
+    def checkCorr(self, st: int, col: int, board: List[List[str]]) -> bool:
+        res = True
+        # in one row
+        for i_col in range(9):
+            if(i_col == col):
+                continue
+            else:
+                if(board[st][col] == board[st][i_col]):
+                    res = False
+                    break
+        # in one col
+        for i_row in range(9):
+            if(i_row == st):
+                continue
+            else:
+                if(board[st][col] == board[i_row][col]):
+                    res = False
+                    break
+        return res
+    
+if __name__ == "__main__":
+    
+    solution = ValidTester()
+    
+    # Test for some parts
+    
+    
+    
+    
