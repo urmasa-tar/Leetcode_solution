f5675a4c1d751d611c1c2aaa204b02fa96eaecc5
urmasa <nik.goltsev.97@bk.ru>
2025-04-29 00:22:15 +0300

    3Sum

--- Process_simulation/3Sum.py
+++ Process_simulation/3Sum.py
@@ -8,10 +8,30 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = [[]]
         loc_nums = nums.sort()
+        print(loc_nums)
         # 
         return res
     
-    def find_Valid_with_ij(self, nums) -> List[List[int]]:
+    def find_Valid_with_ij(self, i, j, nums: List[int]) -> List[List[int]]:
         res = []
+        # find midle by i and j
+        mid = (i + j) // 2
+        # -1 - got down 1 - go up
+        grad_val = 0
+        while(True):
+            if(mid == i) or (mid == j):
+                break
+            elif():
+            
+            elif():
+            
+            else:
 
         return res
+
+
+if __name__ == "__main__":
+
+    to_calc = Solution()
+
+    
