4872ea39a497a55045049418a603bd41b7be803c
urmasa <nik.goltsev.97@bk.ru>
2025-04-29 00:14:02 +0300

    3Sum

---
+++ Process_simulation/3Sum.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        loc_nums = nums.sort()
+        # 
+        return res
+    
+    def find_Valid_with_ij(self, nums) -> List[List[int]]:
+        res = []
+
+        return res
