f804a273879890c74615736a9dde68d127b3af08
urmasa <nik.goltsev.97@bk.ru>
2025-04-30 10:08:41 +0300

    path_for_File_Update

--- Strings/simpPath.py
+++ Strings/simpPath.py
@@ -10,8 +10,8 @@ class Solution:
         priv = []
         for st in self.arr_of_Paths(path):
             if st == ".." and res != "":
-                res = res - priv[len(priv) - 1]
-                res = res - "/"
+                res = self.last_Substr(res, priv[len(priv) - 1])
+                res = self.last_Substr(res, "/")
                 priv.pop()
             else:
                 priv.append(st)
@@ -21,11 +21,18 @@ class Solution:
         return res
     
     def arr_of_Paths(self, current_Path: str) -> list[str]:
-        
         res = []
-        
         res = list(filter(lambda item: item not in self.arr_to_del, current_Path.split("/")))
-        
+        # print(res)
+        return res
+    
+    def last_Substr(self, st: str, sub: str) -> str:
+        res = ""
+        index = st.rfind(sub)
+        if index != -1 and len(sub) != 1:
+            res = st[:index] + st[index + len(sub) - 1]
+        elif len(sub) == 1:
+            res = st[:index]
         return res
     
 if __name__ == "__main__":
