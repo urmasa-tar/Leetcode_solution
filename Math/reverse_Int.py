2182b700099073021531a5a2e9cffe7fb8e2e831
urmasa <nik.goltsev.97@bk.ru>
2025-07-04 22:40:02 +0300

    Reverse_int

---
+++ Math/reverse_Int.py
@@ -0,0 +1,16 @@
+class Solution:
+
+    def reverse(self, x: int) -> int:
+        res = 0
+        x_cor = x
+        sim_x = -1
+        return res
+
+def main_func():
+
+    cls = Solution()
+    cls.reverse()
+
+if __name__ == "__main__":
+
+    main_func()
