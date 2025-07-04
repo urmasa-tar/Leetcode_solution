6871f33875591c884c9ecc62fb309dcd965966f8
urmasa <nik.goltsev.97@bk.ru>
2025-07-04 23:08:11 +0300

    start_palindrom

---
+++ Math/is_palim.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        res = True
+        
+        return res
+
+    def on_Pose(self, pos: int, num: int) -> int:
+        res = 0
+        
+        return res
+
+def main_fnc() -> bool:
+
+    return True
+
+if __name__ == "__main__":
+    main_fnc()
