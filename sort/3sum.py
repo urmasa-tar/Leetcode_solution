9aa2376d78283a2aa85e1d64f7f4d99954d77a53
urmasa <nik.goltsev.97@bk.ru>
2025-07-18 12:21:20 +0300

    3sum_calc

---
+++ sort/3sum.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+        res = [ ]
+        
+        return res
+    
+def main_func() -> None:
+    
+    sol_class = Solution()
+    
+    return None
+
+if __name__ == "__main__":
+    
+    main_func()
