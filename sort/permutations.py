1941c2ab33ec4d2a7e6804f96c868b90cb525374
urmasa <nik.goltsev.97@bk.ru>
2025-07-21 13:25:10 +0300

    Permutations

---
+++ sort/permutations.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        None
+
+def main_fnc() -> bool:
+
+    return True
+
+if __name__ == "__main___":
+
+    main_fnc()
+        
