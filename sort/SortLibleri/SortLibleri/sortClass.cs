220d864483aa8879bb2899448155ade886dd87b3
urmasa <nik.goltsev.97@bk.ru>
2025-07-22 13:56:39 +0300

    sort_lib project

---
+++ sort/SortLibleri/SortLibleri/sortClass.cs
@@ -0,0 +1,12 @@
+﻿namespace SortLibleri
+{
+    public class sortClass
+    {
+        public sortClass(int[] arr)
+        {
+            private int[] sort_arr = arr;
+            
+        }
+
+    }
+}
