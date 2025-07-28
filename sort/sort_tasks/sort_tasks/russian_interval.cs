404d0ed92d7815b01d2f8e0fbb76d196e3f3a03d
urmasa <nik.goltsev.97@bk.ru>
2025-07-28 13:49:30 +0300

    Comparable structer

--- sort/sort_tasks/sort_tasks/russian_interval.cs
+++ sort/sort_tasks/sort_tasks/russian_interval.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace sort_tasks
 {
+    public struct HightW : IComparable<HightW>
+    {
+        public int Hight;
+        public int Wight;
+
+        public int CompareTo(HightW other)
+        {
+            int hightComprasion = Hight.CompareTo(other.Hight);
+            if (hightComprasion == 0){ hightComprasion = Wight.CompareTo(other.Wight); }
+            return hightComprasion;
+        }
+    }
+
+    public struct WidthH : IComparable<WidthH>
+    {
+        public int Wight;
+        public int Hight;
+
+        public int CompareTo(WidthH other)
+        {
+            int hightComprasion = Wight.CompareTo(other.Wight);
+            if (hightComprasion == 0) { hightComprasion = Hight.CompareTo(other.Hight-); }
+            return hightComprasion;
+        }
+    }
     public class russian_interval
     {
+        //                           env[i][0] - wi env[i][1] - hi
+        public int MaxEnvelopes(int[][] envelopes)
+        {
+            int res = 0;
+
+            HightW[] arr_h_w = new HightW[envelopes.Length];
+
+
+            return res;
+        }
     }
 }
