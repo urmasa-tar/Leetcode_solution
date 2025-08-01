bdf3bd6ef4eec3e3f90763f9c7a16eac1b8de8e9
urmasa <nik.goltsev.97@bk.ru>
2025-08-01 19:47:06 +0300

    check function

--- Strings/string_tasks/string_tasks/regular_expressions.cs
+++ Strings/string_tasks/string_tasks/regular_expressions.cs
@@ -8,18 +8,30 @@ namespace string_tasks
 {
     public class regular_expressions
     {
+        char[] to_check = { '*', '.'};
         public bool IsMatch(string s, string p)
         {
             bool res = false;
-
+            res = rec_from_pointers(s, p, 0, 0, false);
             return false;
         }
 
-        private bool rec_from_pointers(string s, string p, int s_point, int p_point)
+        private bool rec_from_pointers(string s, string p, int s_point, int p_point, bool out_s)
         {
             bool res = false;
 
-            
+            if((s.Length - 1 == s_point) && (p.Length - 1 == p_point))
+            {
+                // check for same
+                
+            }else if ((s.Length - 1 == s_point) && (p.Length - 1 > p_point))
+            {
+                // check for * or . + *
+
+            }else if ((s.Length - 1 > s_point) && (p.Length - 1 == p_point))
+            {
+
+            }
 
             return res;
         }
