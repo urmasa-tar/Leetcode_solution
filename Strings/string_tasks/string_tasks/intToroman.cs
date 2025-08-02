5f01371dcf490dcb52b0cc5be98c43bfe181eb71
urmasa <nik.goltsev.97@bk.ru>
2025-08-02 20:11:51 +0300

    Int to roam start

---
+++ Strings/string_tasks/string_tasks/intToroman.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_tasks
+{
+    public class intToroman
+    {
+        Dictionary<char, int> RomtoInt = new Dictionary<char, int>()
+        {
+            {'M', 1000},
+            {'D', 500},
+            {'C', 100},
+            {'L', 50},
+            {'X', 10},
+            {'V', 5},
+            {'I', 1}
+        };
+        public String intToRoman(int num)
+        {
+            string res = "";
+            int i = 0;
+            int ln_d = RomtoInt.Count;
+            while (num > 0)
+            {
+                if(i >= ln_d) { break; }
+
+            }
+
+            return res;
+        }
+    }
+}
