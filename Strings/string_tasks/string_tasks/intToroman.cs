dfed65f2f995c1245613a24f9501f053bca322f4
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-08-04 23:58:50 +0300

    Solution options

--- Strings/string_tasks/string_tasks/intToroman.cs
+++ Strings/string_tasks/string_tasks/intToroman.cs
@@ -20,16 +20,41 @@ namespace string_tasks
         };
         public String intToRoman(int num)
         {
+            int correct_num = num;
             string res = "";
             int i = 0;
             int ln_d = RomtoInt.Count;
             while (num > 0)
             {
                 if(i >= ln_d) { break; }
-                if (RomtoInt[i].Value > )
+                if (RomtoInt.ElementAt(i).Value <= correct_num)
                 {
-
+                    correct_num -= RomtoInt.ElementAt(i).Value;
+                    res += RomtoInt.ElementAt(i).Key;
                 }
+                else
+                {
+                    for(int j = i + 1; j < ln_d; j++)
+                    {
+                        if(RomtoInt.ElementAt(i).Value - RomtoInt.ElementAt(j).Value <= correct_num)
+                        {
+                            correct_num -= RomtoInt.ElementAt(i).Value - RomtoInt.ElementAt(j).Value;
+                            res += RomtoInt.ElementAt(i).Key + RomtoInt.ElementAt(j).Key;
+                        }
+                        if(RomtoInt.ElementAt(i).Value - (RomtoInt.ElementAt(j).Value * 2) <= correct_num)
+                        {
+                            correct_num = correct_num - RomtoInt.ElementAt(i).Value + RomtoInt.ElementAt(j).Value * 2;
+                            res += RomtoInt.ElementAt(j).Key * 2 + RomtoInt.ElementAt(i).Key;
+                        }
+                        if(RomtoInt.ElementAt(i).Value - (RomtoInt.ElementAt(j).Value * 3) <= correct_num)
+                        {
+                            correct_num = correct_num - RomtoInt.ElementAt(i).Value + RomtoInt.ElementAt(j).Value;
+                            res += RomtoInt.ElementAt(j).Key * 3 + RomtoInt.ElementAt(i).Key;
+                        }
+                    }
+                }
+                if (RomtoInt.ElementAt(i).Value > correct_num) { i += 1; }
+                if (correct_num == 0) { break; }
             }
 
             return res;
---
+++ Strings/string_tasks/string_tasks/updateRomToInr.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_tasks
+{
+    public class updateRomToInr
+    {
+        Dictionary<int, string> IntToRom = new Dictionary<int, string>
+        {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"} 
+        };
+
+        public string IntToRoman(int num)
+        {
+            StringBuilder roman = new StringBuilder();
+
+            foreach (var kvp in IntToRom.OrderByDescending(x => x.Key))
+            {
+                while (num >= kvp.Key)
+                {
+                    roman.Append(kvp.Value);
+                    num -= kvp.Key;
+                }
+            }
+
+            return roman.ToString();
+        }
+    }
+}
