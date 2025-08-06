85e01cbeba73f756b01b085ef534991d0902c3ae
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-08-06 05:50:33 +0300

    substring prefix

--- Strings/string_tasks/string_tasks/prefixFind.cs
+++ Strings/string_tasks/string_tasks/prefixFind.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,18 +11,44 @@ namespace string_tasks
     {
         public struct strringWithLebgth : IComparable<strringWithLebgth>
         {
-            public int Length;
+            public int ln;
             public string str;
 
             public int CompareTo(strringWithLebgth other)
             {
-                return Length.CompareTo(other.Length);
+                return ln.CompareTo(other.ln);
             }
         }
         public string LongestCommonPrefix(string[] strs)
         {
             string res = "";
+            strringWithLebgth[] string_ln = new strringWithLebgth[strs.Length];
+            for (int i = 0; i < strs.Length; i++)
+            {
+                string_ln[i].str = strs[i];
+                string_ln[i].ln = strs[i].Length;
+            }
+            Array.Sort(string_ln);
+            for(int pref = 0; pref < string_ln[0].ln; pref++)
+            {
+                if(ComparePref(pref, string_ln)) { res = string_ln[0].str.Substring(0, pref); }
+                else { break; }
+            }
+            return res;
+        }
 
+        private bool ComparePref(int pref, strringWithLebgth[] strs)
+        {
+            bool res = true;
+            for(int str_n = 0; str_n < strs.Length; str_n++)
+            {
+                if (strs[0].str.AsSpan(0, pref).SequenceEqual(strs[str_n].str.AsSpan(0, pref))) { continue; }
+                else
+                {
+                    res = false;
+                    break;
+                }
+            }
             return res;
         }
     }
