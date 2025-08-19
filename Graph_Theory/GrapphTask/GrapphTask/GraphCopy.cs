185da9d9ce10a3c7341e6e32ee2e64cd182f09c7
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-08-19 13:18:42 +0300

    Copy of task

---
+++ Graph_Theory/GrapphTask/GrapphTask/GraphCopy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapphTask
+{
+    public class GraphCopy
+    {
+        public class Node
+        {
+            public int val;
+            public IList<Node> neighbors;
+
+            public Node()
+            {
+                val = 0;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val)
+            {
+                val = _val;
+                neighbors = new List<Node>();
+            }
+
+            public Node(int _val, List<Node> _neighbors)
+            {
+                val = _val;
+                neighbors = _neighbors;
+            }
+        }
+
+        public Node CloneGraph(Node node)
+        {
+            Node res_node = new Node();
+
+            return res_node;
+        }
+    }
+}
---
+++ Graph_Theory/GrapphTask/GrapphTask/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GrapphTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
