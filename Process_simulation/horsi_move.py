ef51f07533f324cf99929f11d10d5d77e624c5f5
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-11 15:51:57 +0300

    horsi move in boards

--- Process_simulation/horsi_move.py
+++ Process_simulation/horsi_move.py
@@ -12,11 +12,13 @@ def sum_of_ways(n, m):
     pos_x = 0
     pos_y = 0
 
-    return rec_from_point() + rec_from_point()
+    return rec_from_point(pos_y + 2, pos_x + 1, n, m) + rec_from_point(pos_y + 1, pos_x + 2, n, m)
 
-def rec_from_point() -> int:
+def rec_from_point(y: int, x: int, n: int, m: int) -> int:
     res = 0
-
+    if(is_in_board(n, m, y, x) and ((y != (n - 1)) and x != (m - 1))):
+        res += rec_from_point(y + 2, x + 1, n, m) + rec_from_point(y + 1, x + 2, n, m)
+    elif((y == (n - 1)) and (x == (m - 1))): res += 1
     return res
 
 def is_in_board(n, m, y, x) -> bool:
