e1d5356aabf3ba645f7e93a98e45b1bf56ce1fb7
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-16 22:39:06 +0300

    js parser start

---
+++ hashTables/js_code_parser.py
@@ -0,0 +1,24 @@
+import sys
+
+
+def main():
+    
+    num_bind = int(input())
+    arr_bind = input().split()
+    
+    # print(arr_bind)
+    file_ln = int(input)
+    arr_of_file_str = [] 
+    for i in range(file_ln):
+        str_file = input().split()
+        arr_of_file_str.append(str_file)
+    
+    
+
+def pars_file() -> str:
+    res = ""
+
+    return res
+
+if __name__ == '__main__':
+    main()
