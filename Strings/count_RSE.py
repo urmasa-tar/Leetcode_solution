84a33e7327cc89cd9d1b142eb8aec4212eac933a
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-23 18:57:18 +0300

    update

---
+++ Strings/count_RSE.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    
+    def countAndSay(self, n: int) -> str:
+        res = ""
+        arr = []
+        
+
+
+        return res
+    
+    def int_to_arr(self, num: int) -> List[int]:
+        res_arr = []
+
+        return res_arr
+
+    def pair_to_str(self) -> str:
+        res = ""
+
+        return res
+
+    def sub_str_decode(self) -> tuple[int, str]:
+        res_substr = ""
+        new_ind = 0
+        return res_substr, new_ind
+
+if __name__ == "__main__":
+
+    prog = Solution()
