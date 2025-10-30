bc18c18471036d09d6d4e12059443e1890165282
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-30 10:48:05 +0300

    Container calc for biggest boards

--- Math/bidest_container.py
+++ Math/bidest_container.py
@@ -5,12 +5,26 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         
         res = 0
+        i = 0
+        j = len(height) - 1
+        while(True):
+            if(i >= j):
+                break
+            current = self.calc_Pull(i, j, min(height[i], height[j]))
+            if(current > res):
+                res = current
+            if(height[i] => height[j]):
+                j -= 1
+            else:
+                i += 1
 
-        return 0
+        return res
     
     def calc_Pull(self, start: int, end: int, min_board: int) -> int:
 
-        return 0
+        res = (end - start) * min_board
+
+        return res
 
     
 
