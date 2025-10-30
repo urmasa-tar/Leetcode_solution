785939e0e4741fb45632f96fef940c2122d88a13
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-30 10:56:31 +0300

    String pattern creation

---
+++ Strings/pattern_match.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+        
+        res = False
+
+        return res
+
+    def rec_check_pattern(self) -> bool:
+        res = False
+        # switch case for 0 - False (1 - equal or 1 - ?) 2 - * 
+        return res
+    
+    def is_equal_part() -> int:
+        res = 0
+
+        return res
+
+
+if __name__ == "__main__":
+
+    prog_test = Solution()
