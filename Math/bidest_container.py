8c09187a11bba69a42a156a3871b68085b4924bb
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-10-30 10:27:53 +0300

    Container start

---
+++ Math/bidest_container.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+
+    def maxArea(self, height: List[int]) -> int:
+        
+        res = 0
+
+        return 0
+    
+    def calc_Pull(self, start: int, end: int, min_board: int) -> int:
+
+        return 0
+
+    
+
+if __name__ == "__main__":
+
+    prog_test = Solution()
