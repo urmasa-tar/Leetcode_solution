8f98622d06c56d7bf7ba65719760b5174337c91f
Nikita Goltsev <68222851+urmasa-tar@users.noreply.github.com>
2025-11-16 22:26:58 +0300

    base start for data structer

---
+++ Data_Structers/Data_Structers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_Structers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Data_Structers/Data_Structers/array_structer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structers
+{
+    public class array_structer
+    {
+        array_structer() { }
+
+
+    }
+}
