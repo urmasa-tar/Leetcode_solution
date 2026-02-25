80d750374855e793b19794e7217e364b3358b417
urmasa-tar <nik.goltsev.97@bk.ru>
2026-02-25 17:28:08 +0300

    calendar start

--- cpp_YandexGuide/first_steps/day_in_month.cpp
+++ cpp_YandexGuide/first_steps/day_in_month.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
+int month_check(int month, int year);
+bool check_of_year(int y);
+
 int main(){
+
+    int month, year;
+    std::cin >> month >> year;
+
+    std::cout << month_check(month, year) << "\n";
 
     return 0;
 }
 
-int month_check(){
-    int res = 0;
+int month_check(int month, int year){
+    int res = 31;
+
+    bool is_vsc = check_of_year(year);
+    if(month == 2 && is_vsc){
+        res = 29;
+    }else if (month == 2){ res = 28; }
+    else if ( (month < 7) && (month % 2 == 0)){ res = 30; }
+    else if ( (month > 7) && (month % 2 == 1)){ res = 30; }
 
     return res;
 }
